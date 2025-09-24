530a696a13f7fdd76199e9754f1f083760488540
Andre van Tonder <andre@drevan.me>
2025-09-24 21:47:39 +1000

    Add compat date

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -25,6 +25,7 @@ export default defineNuxtConfig({
     cloudflare: {
       deployConfig: true,
       wrangler: {
+        compatibility_date: '2025-09-24',
         d1_databases: [
           {
             binding: 'DB',
